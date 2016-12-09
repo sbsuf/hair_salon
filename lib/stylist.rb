353cfeb7ed7f303b6ecdd280cc346ab6f2d59f42
sbsuf <justinjxu@yahoo.com>
2016-12-08 18:00:15 -0800

    add all method into Stylist class

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -8,6 +8,13 @@ class Stylist
 
   def self.all()
     returned_stylists = DB.exec("SELECT * FROM stylists;")
+    stylists = []
+    returned_stylists.each() do |stylist|
+      name = stylist.fetch("name")
+      id = stylist.fetch("id").to_i()
+      stylists.push(Stylist.new({name: name, id: id}))
+    end
+    stylists
   end
 
 

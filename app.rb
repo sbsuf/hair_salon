e370564ba7c70af0e0fa04e2932b4c8916c35cd0
sbsuf <justinjxu@yahoo.com>
2016-12-08 19:01:27 -0800

    create index and stylist_success.erb

---
+++ app.rb
@@ -0,0 +1,23 @@
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/stylist")
+require('./lib/list')
+require("pg")
+
+DB = PG.connect({:dbname => "hair_salon"})
+
+get("/") do
+  erb(:index)
+end
+
+get("/stylists/new") do
+  erb(:index)
+end
+
+post("/stylists") do
+  name = params.fetch("name")
+  stylist = Stylist.new({:name => name, :id => nil})
+  stylist.save()
+  erb(:stylist_success)
+ end
